4a3f7b355fc726a2281c6e1d1c56088d7f75cbab
Jeremiah Giordani <jeremiah@dynamic-oit-ip4-wifirestricted03-10-48-61-176.princeton.edu>
2024-12-06 09:10:31 -0500

    using local pretrained weights

--- pretrained_model.py
+++ pretrained_model.py
@@ -2,9 +2,13 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
+pretrained_weights = "/home/jg0037/.cache/torch/hub/checkpoints/resnet18-f37072fd.pth"
+
 def get_pretrained_model(num_classes=4):
     # Example: Use ResNet18 pretrained on ImageNet
-    model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+    weights = torch.load(pretrained_weights)
+    model = models.resnet18()
+    model.load_state_dict(weights)
     # Replace the final layer
     in_features = model.fc.in_features
     model.fc = nn.Linear(in_features, num_classes)
